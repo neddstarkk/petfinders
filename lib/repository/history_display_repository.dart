f0b310a2b7288461ea34e902d22d6a1a21abf3f9
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2022-07-28 19:14:46 +0530

    Beautified the History Screen

--- lib/repository/history_display_repository.dart
+++ lib/repository/history_display_repository.dart
@@ -5,15 +5,15 @@ import 'package:petfinders/models/pet_display_model.dart';
 import 'package:petfinders/repository/home_display_repository.dart';
 
 class HistoryDisplayRepository {
-  List<PetDisplayModel> displayItems = [];
-
   List<PetDisplayModel> populateHistory(BuildContext context) {
-    HomeDisplayRepository list = BlocProvider.of<PetAdoptionCubit>(context).repo;
+    List<PetDisplayModel> displayItems = [];
+    HomeDisplayRepository list =
+        BlocProvider.of<PetAdoptionCubit>(context).repo;
 
     var newList = list.availablePetsForAdoption;
 
     for (int i = 0; i < newList.length; i++) {
-      if(newList[i].adopted) {
+      if (newList[i].adopted) {
         displayItems.add(newList[i]);
       }
     }
--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:petfinders/models/pet_display_model.dart';
 import 'package:petfinders/repository/history_display_repository.dart';
+import 'package:petfinders/util/size_config.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -16,19 +17,58 @@ class _HistoryScreenState extends State<HistoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     // This violates the SOLID principle
     display = repo.populateHistory(context);
-    
+
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          "History of Adoptions",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
       body: display.isEmpty
           ? Center(
               child: Text("No Pets adopted yet"),
             )
           : ListView.builder(
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(display[index].name),
+                return Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade200),
+                  margin: EdgeInsets.symmetric(
+                      horizontal: SizeConfig.blockSizeHorizontal! * 7, vertical: SizeConfig.blockSizeVertical!),
+                  height: SizeConfig.screenHeight! / 9,
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      height: SizeConfig.screenHeight! / 9,
+                      width: 120,
+                      child: Image(
+                        image: display[index].image,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          display[index].name,
+                          style: const TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Text(display[index].breed, style: TextStyle(color: Colors.grey),),
+                      ],
+                    )
+                  ]),
                 );
               },
               itemCount: display.length,
